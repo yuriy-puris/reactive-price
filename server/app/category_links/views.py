b4204dd3fca80478b2d75977d3c53bf4b7da34b1
Yuriy Puris <ypuris@itera-research.com>
2019-10-08 15:40:00 +0300

    category links view

--- server/app/category_links/views.py
+++ server/app/category_links/views.py
@@ -1,7 +1,23 @@
+import requests
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import CategoryLinks
 from .serializers import CategoryLinksSerializer
 
-class CategoryLinksView(generics.ListAPIView):
-  queryset = CategoryLinks.objects.all()
-  serializer_class = CategoryLinksSerializer
+class CategoryLinksView(APIView):
+
+  def get(self, request, format=None):
+    queryset = CategoryLinks.objects.all()
+    serializer_class = CategoryLinksSerializer(queryset, many=True)
+    return Response(serializer_class.data)
+
+  def post(self, request, format=None):
+    queryset = CategoryLinks.objects.all()
+    serializer = CategoryLinksSerializer(queryset, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
